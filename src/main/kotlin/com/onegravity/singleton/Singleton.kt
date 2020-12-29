0ba7ceb44ce5a602778ecc7784376a2c5e22e544
Emanuel Moecklin <1gravityllc@gmail.com>
2020-12-29 16:33:01 -0500

    Singleton, the easiest one

---
+++ src/main/kotlin/com/onegravity/singleton/Singleton.kt
@@ -0,0 +1,15 @@
+package com.onegravity.singleton
+
+/**
+ * Singleton's are part of the Kotlin language. It can't get any simpler than this
+ */
+object Singleton
+
+/**
+ * The "Java equivalent" version of an Singleton design pattern using a private constructor and lazy initialization for the instance.
+ */
+class Singleton2 private constructor() {
+    companion object {
+        val instance by lazy { Singleton2() }
+    }
+}
